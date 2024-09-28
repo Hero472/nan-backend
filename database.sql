bd89b5e0090c14304636a37238d6fcc478b4f0f1
Hero472 <herodr@outlook.cl>
2024-09-28 14:53:12 -0300

    database updated

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-CREATE TYPE LevelEnum AS ENUM ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'); 
+CREATE TYPE LevelEnum AS ENUM ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12','13'); 
 CREATE TYPE DayEnum AS ENUM ('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'); 
 CREATE TYPE BlockEnum AS ENUM ('A', 'B', 'C', 'D', 'E');
 
